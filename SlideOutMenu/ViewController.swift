0fe29488bc0bd77981fa688fe873a70b29463509
Timur Cheberda <likedune@gmail.com>
2019-04-03 23:26:47 +0300

    Initial Commit

---
+++ SlideOutMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideOutMenu
+//
+//  Created by Тимур Чеберда on 03/04/2019.
+//  Copyright © 2019 Tmur Cheberda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
