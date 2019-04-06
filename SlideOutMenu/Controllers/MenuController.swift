d1f851d4d9dc8fdaa27889b45cdca39db5d58c97
Timur Cheberda <likedune@gmail.com>
2019-04-06 16:20:04 +0300

    Update MenuController.swift

--- SlideOutMenu/Controllers/MenuController.swift
+++ SlideOutMenu/Controllers/MenuController.swift
@@ -13,18 +13,8 @@ class MenuController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.backgroundColor = .blue
-        
-        //        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        //        self.view.addGestureRecognizer(panGesture)
     }
-    
-    //    @objc func handlePan(gesture: UIPanGestureRecognizer) {
-    //        let translation = gesture.translation(in: view)
-    //        let x = translation.x + 300
-    //        view.transform = CGAffineTransform(translationX: x, y: 0)
-    //        // try to fetch the Red Home Controller through the main keywindow somehow
-    //        // if you try to do this, your code will be a little crazy and very very unmaintainable
-    //    }
+
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
